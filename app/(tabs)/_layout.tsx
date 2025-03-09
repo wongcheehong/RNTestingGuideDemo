a14631fb1ed0814ae97e43ca3d35441ff595ad78
Felix Wong Chee Hong <cheehong.wong@ytldigital.com>
2025-03-09 17:50:13 +0800

    refactor: rename file

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -39,7 +39,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="testing"
+        name="testing-demo"
         options={{
           title: 'Testing Demo',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
